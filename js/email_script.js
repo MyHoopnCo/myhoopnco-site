5fd405217517ac1bcd85ae59fb6e0e868591b763
Tioluwani <43251329+t1oluwani@users.noreply.github.com>
2026-02-19 12:50:28 -0700

    fix: sorted out emails not saving issues

---
+++ js/email_script.js
@@ -0,0 +1,34 @@
+const googleSheetsURL =
+  "https://script.google.com/macros/s/AKfycbwXBm62VWog2tXTAidiv_hWTtTHTHbfeoBsnKEcB-NFCyBbbQJWbWWbbeGuCVAyxHYN/exec";
+
+const ctaForm = document.getElementById("ctaForm");
+const message = document.getElementById("message");
+
+ctaForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const email = document.getElementById("email").value.trim();
+
+  fetch(googleSheetsURL, {
+    method: "POST",
+    body: JSON.stringify({ email }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.result === "success") {
+        message.innerText = "You're on the list!";
+        message.style.color = "green";
+      } else if (data.result === "duplicate") {
+        message.innerText = "You're already on the list.";
+        message.style.color = "orange";
+      } else {
+        message.innerText = "Something went wrong.";
+        message.style.color = "red";
+      }
+      ctaForm.reset();
+    })
+    .catch(() => {
+      message.innerText = "Server error. Try again.";
+      message.style.color = "red";
+    });
+});
